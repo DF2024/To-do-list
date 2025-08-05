b0803686327ffda70421de5a978e0daf21de2555
DF2024 <ad79702@gmail.com>
2025-08-05 12:12:35 -0400

    Se crearon las vistas de la aplicación

--- todolist/tareas/views.py
+++ todolist/tareas/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+
+class TaskUptadte(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'completed']
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
